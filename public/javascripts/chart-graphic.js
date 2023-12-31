f0d659f2d2d632e659b501f7b630984ee2e7ad5e
Alan Tavares Morais <pollargithub@gmail.com>
2023-12-30 21:49:11 -0300

    alterando estilo de grafico

--- public/javascripts/chart-graphic.js
+++ public/javascripts/chart-graphic.js
@@ -1,32 +1,36 @@
 'use strict';
 
-const ctx = document.getElementById('myChart');
-const data = {
-    labels: ['junior', 'Pleno', 'Senior'],
-    datasets: [
-        {
-            label: 'Média mensal',
-            data: [3000, 5500, 7049],
-            borderWidth: 1,
-            backgroundColor: [
-                '#089fdd',
-                "#0867db",
-                "#0808d8"
-            ],
-            borderRadius: 15,
-        }
-    ]
-}
+const ctx = document.getElementById('myChart').getContext('2d');
 
 new Chart(ctx, {
-    type: 'bar',
-    data: data,
+    // type: 'bar',
+    data: {
+        labels: ['junior', 'Pleno', 'Senior'],
+        datasets: [
+            {
+                type: 'bar',
+                label: 'Média salarial mensal',
+                data: [
+                    3000,
+                    5500,
+                    7049
+                ],
+                borderWidth: 1,
+                backgroundColor: [
+                    '#089fdd',
+                    // "#0867db",
+                    // "#0808d8"
+                ],
+                borderRadius: 15,
+            },
+        ]
+    },
     options:{
         scales: {
             y:{
                 beginAtZero: true
             }
-        }
+        },
     }
     
 });
