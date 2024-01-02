d80ce8072217ecdf1f559ca0fb5b88ea2d420486
Alan Tavares Morais <pollargithub@gmail.com>
2024-01-02 15:23:44 -0300

    alteração preco

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -510,6 +510,7 @@ $svgIconBox.each(function() {
         $svgIcon = $this.find('.svg-icon'),
         $id = $svgIcon.attr('id'),
         $icon = $svgIcon.data('svg-icon');
+        console.log($id);
     var $vivus = new Vivus($id, { duration: 180, file: $icon });
     $this.on('mouseenter', function () {
         $vivus.reset().play();
