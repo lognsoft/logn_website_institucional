e4a1a2dc1c32469e58b0c9b220363172078392e4
Alan Tavares Morais <pollargithub@gmail.com>
2024-02-20 18:26:34 -0300

    repaginando página de curso online

---
+++ public/javascripts/contadorDeAulas.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function contarAulas(){
+    let containerContador = document.querySelector("#contador-aulas");
+    let teamDetails = document.querySelectorAll(".accordion .tg__animate-text");
+    
+    containerContador.innerHTML = teamDetails.length
+}
+window.addEventListener("load", contarAulas);
