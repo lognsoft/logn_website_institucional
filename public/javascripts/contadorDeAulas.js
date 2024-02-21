f36f8a08229fd064dc129cfac3b0a585db21a818
Alan Tavares Morais <pollargithub@gmail.com>
2024-02-21 11:19:55 -0300

    correção de bug na página da plataforma - mobile

--- public/javascripts/contadorDeAulas.js
+++ public/javascripts/contadorDeAulas.js
@@ -1,7 +1,6 @@
 function contarAulas(){
     let containerContador = document.querySelector("#contador-aulas");
-    let teamDetails = document.querySelectorAll(".accordion .tg__animate-text");
-    
+    let teamDetails = document.querySelectorAll(".accordion .active");
     containerContador.innerHTML = teamDetails.length
 }
 window.addEventListener("load", contarAulas);
